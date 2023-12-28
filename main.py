44be98cec2efd874382767a999fec80412ee16c7
githubkingforever2nd <154126061+githubkingforever2nd@users.noreply.github.com>
2023-12-28 12:09:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,296 +1,186 @@
 import pyrogram
 from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserAlreadyParticipant, InviteHashExpired
+from pyrogram.types import Message
 
 import time
 import os
 import threading
-import json
 
-with open('config.json', 'r') as f: DATA = json.load(f)
-def getenv(var): return os.environ.get(var) or DATA.get(var, None)
-
-bot_token = "6570166456:AAE0cPk6-LaE7LYwBt_jIyyZ1-TAkwCVMCw" 
-api_hash = "1d50ef27561815931234223886485498" 
-ss = "BQHGoKcAXkyQzW6CUheuFiv2HPUYdb-it9iHVYPVpWOo7Mq_Z7qb1otPSjXFrc5sSdCtKChdsDDthPrOxbiKZHEYf4gjsP7xKlE0a3uh3xIdYfiAaraZTpGN36VEBMM2t2SfqssvphOxMk_LRDY3zqHx0XLp5VfQyXaQ5R9EWRoiE-vyWHwVW_qIksoRqx3XAskq73Z7TQilVH416mLfEdYW4vCKNsPm158JEFNS37H3iPg12N4XPh4-5FFsCx4sHVGte66jgPyP1HAklphfG2bEDyfvt8UIfWzXn422Ljnksq5i2XG5RcSX4n2F_US2rpbNTHneMqyu077-BZZWYM1qXrkHMQAAAAF3NzZJAA"
+bot_token = "6570166456:AAE0cPk6-LaE7LYwBt_jIyyZ1-TAkwCVMCw"
+api_hash = "1d50ef27561815931234223886485498"
 api_id = int(29794471)
-bot = Client("mybot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+ss = "BQHGoKcAXkyQzW6CUheuFiv2HPUYdb-it9iHVYPVpWOo7Mq_Z7qb1otPSjXFrc5sSdCtKChdsDDthPrOxbiKZHEYf4gjsP7xKlE0a3uh3xIdYfiAaraZTpGN36VEBMM2t2SfqssvphOxMk_LRDY3zqHx0XLp5VfQyXaQ5R9EWRoiE-vyWHwVW_qIksoRqx3XAskq73Z7TQilVH416mLfEdYW4vCKNsPm158JEFNS37H3iPg12N4XPh4-5FFsCx4sHVGte66jgPyP1HAklphfG2bEDyfvt8UIfWzXn422Ljnksq5i2XG5RcSX4n2F_US2rpbNTHneMqyu077-BZZWYM1qXrkHMQAAAAF3NzZJAA"
+bot = Client("mybot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
+acc = Client("myacc",api_id=api_id,api_hash=api_hash,session_string=ss)
 
-ss = "BQAYezAABZF39PrvgyidFyaU5vPyBkeuMXlVBkU2pTTOlMDEO4S2MqcdYEVfHsOct6S_z6ZxmepB5ss9S8SZAkkwyxomIvUQLDI8DMdKDJqcma7Q-HNfDay0WtAUMKlxRql-NtCV2P22DWKUlWQk4uuEBQ2xs1nyKJtyajl-ePiMpj3Dzq4I7NcZk_RCAFpVpHHYcF_GjwxXwMXfbor-hftCbwtVR59jIPQzkoJMU-PKLanSrBcotJ_9mIzKZCcEjwANtbrIBaL5FldOg6PtfCHQ4vXPn6s9PtzvVP6p5DZoy5HqTSb35RrPtaONtXRz6OvGs76-Lt3k6psfm0dNiYXdAGUL7AAAAAGGJj8xAA"
-if ss is not None:
-	acc = Client("myacc" ,api_id=api_id, api_hash=api_hash, session_string=ss)
-	acc.start()
-else: acc = None
+
+@bot.on_message(filters.command(["start"]))
+async def account_login(bot: Client, m: Message):
+ editable = await m.reply_text("**I am a simple save restricted bot**.\n\nSend message link to clone/download here\n Must join:- @Bypass_restricted")
+@bot.on_message(filters.command(["bulk"]))
+async def account_login(bot: Client, m: Message):
+ editable = await m.reply_text("**I am not an advanced bot")
 
 # download status
 def downstatus(statusfile,message):
-	while True:
-		if os.path.exists(statusfile):
-			break
+    while True:
+        if os.path.exists(statusfile):
+            break
 
-	time.sleep(3)      
-	while os.path.exists(statusfile):
-		with open(statusfile,"r") as downread:
-			txt = downread.read()
-		try:
-			bot.edit_message_text(message.chat.id, message.id, f"__Downloaded__ : **{txt}**")
-			time.sleep(10)
-		except:
-			time.sleep(5)
+    time.sleep(3)      
+    while os.path.exists(statusfile):
+        with open(statusfile,"r") as downread:
+            txt = downread.read()
+        try:
+            bot.edit_message_text(message.chat.id, message.id, f"__Downloaded__ : **{txt}**")
+            time.sleep(10)
+        except:
+            time.sleep(5)
 
 
 # upload status
 def upstatus(statusfile,message):
-	while True:
-		if os.path.exists(statusfile):
-			break
+    while True:
+        if os.path.exists(statusfile):
+            break
 
-	time.sleep(3)      
-	while os.path.exists(statusfile):
-		with open(statusfile,"r") as upread:
-			txt = upread.read()
-		try:
-			bot.edit_message_text(message.chat.id, message.id, f"__Uploaded__ : **{txt}**")
-			time.sleep(10)
-		except:
-			time.sleep(5)
+    time.sleep(3)      
+    while os.path.exists(statusfile):
+        with open(statusfile,"r") as upread:
+            txt = upread.read()
+        try:
+            bot.edit_message_text(message.chat.id, message.id, f"__Uploaded__ : **{txt}**")
+            time.sleep(10)
+        except:
+            time.sleep(5)
 
 
 # progress writter
 def progress(current, total, message, type):
-	with open(f'{message.id}{type}status.txt',"w") as fileup:
-		fileup.write(f"{current * 100 / total:.1f}%")
-
-
-# start command
-@bot.on_message(filters.command(["start"]))
-def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Save-Restricted-Bot")]]), reply_to_message_id=message.id)
+    with open(f'{message.id}{type}status.txt',"w") as fileup:
+        fileup.write(f"{current * 100 / total:.1f}%")
 
 
 @bot.on_message(filters.text)
 def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	print(message.text)
 
-	# joining chats
-	if "https://t.me/+" in message.text or "https://t.me/joinchat/" in message.text:
+    # joining chats
+    if "https://t.me/+" in message.text or "https://t.me/joinchat/" in message.text:
 
-		if acc is None:
-			bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
-			return
+        try:
+            with acc:
+                acc.join_chat(message.text)
+            bot.send_message(message.chat.id,"**successfully join the chat**", reply_to_message_id=message.id)
+        except UserAlreadyParticipant:
+            bot.send_message(message.chat.id,"**successfully join the chat**", reply_to_message_id=message.id)
+        except InviteHashExpired:
+            bot.send_message(message.chat.id,"**link has expired.**", reply_to_message_id=message.id)
+    
+    # getting message
+    elif "https://t.me/" in message.text:
 
-		try:
-			try: acc.join_chat(message.text)
-			except Exception as e: 
-				bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=message.id)
-				return
-			bot.send_message(message.chat.id,"**Chat Joined**", reply_to_message_id=message.id)
-		except UserAlreadyParticipant:
-			bot.send_message(message.chat.id,"**Chat alredy Joined**", reply_to_message_id=message.id)
-		except InviteHashExpired:
-			bot.send_message(message.chat.id,"**Invalid Link**", reply_to_message_id=message.id)
+        datas = message.text.split("/")
+        msgid = int(datas[-1])
 
-	# getting message
-	elif "https://t.me/" in message.text:
+        # private
+        if "https://t.me/c/" in message.text:
+            chatid = int("-100" + datas[-2])
 
-		datas = message.text.split("/")
-		temp = datas[-1].replace("?single","").split("-")
-		fromID = int(temp[0].strip())
-		try: toID = int(temp[1].strip())
-		except: toID = fromID
+            with acc:
+                msg  = acc.get_messages(chatid,msgid)
 
-		for msgid in range(fromID, toID+1):
+                if "text" in str(msg):
+                    bot.send_message(message.chat.id, msg.text, entities=msg.entities, reply_to_message_id=message.id)
+                    return
 
-			# private
-			if "https://t.me/c/" in message.text:
-				chatid = message.text.split("/")[-2]
-            	print(chatid)
-           	 	if chatid.isdigit() == True:
-                	if len(chatid) > 3:
-						chatid = int('-100' + str(message.text.split("/")[-2]))
-                    	print(chatid)
-                	else:
-                    	chatid =int('-100' + str(message.text.split("/")[-3]))
-                    	print(chatid)
-           		else:
-                	chatid = await client.get_chat(message.text.split("/")[-2])
-                	chatid = chat.id
-                	print(chatid)
-				#chatid = int("-100" + datas[4])
-				
-				if acc is None:
-					bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
-					return
-				
-				handle_private(message,chatid,msgid)
-				# try: handle_private(message,chatid,msgid)
-				# except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=message.id)
-			
-			# bot
-			elif "https://t.me/b/" in message.text:
-				username = datas[4]
-				
-				if acc is None:
-					bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
-					return
-				try: handle_private(message,username,msgid)
-				except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=message.id)
+                smsg = bot.send_message(message.chat.id, '__Downloading__', reply_to_message_id=message.id)
+                dosta = threading.Thread(target=lambda:downstatus(f'{message.id}downstatus.txt',smsg),daemon=True)
+                dosta.start()
+                file = acc.download_media(msg, progress=progress, progress_args=[message,"down"])
+                os.remove(f'{message.id}downstatus.txt')
 
-			# public
-			else:
-				username = datas[3]
+                upsta = threading.Thread(target=lambda:upstatus(f'{message.id}upstatus.txt',smsg),daemon=True)
+                upsta.start()
 
-				try: msg  = bot.get_messages(username,msgid)
-				except UsernameNotOccupied: 
-					bot.send_message(message.chat.id,f"**The username is not occupied by anyone**", reply_to_message_id=message.id)
-					return
+            if "Document" in str(msg):
+                try:
+                    with acc:
+                        thumb = acc.download_media(msg.document.thumbs[0].file_id)
+                except:
+                    thumb = None
+                bot.send_document(message.chat.id, file, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
+                if thumb != None:
+                    os.remove(thumb)
 
-				try: bot.copy_message(message.chat.id, msg.chat.id, msg.id,reply_to_message_id=message.id)
-				except:
-					if acc is None:
-						bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
-						return
-					try: handle_private(message,username,msgid)
-					except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=message.id)
+            elif "Video" in str(msg):
+                try:
+                    with acc:
+                        thumb = acc.download_media(msg.video.thumbs[0].file_id)
+                except:
+                    thumb = None
+                bot.send_video(message.chat.id, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
+                if thumb != None:
+                    os.remove(thumb)
 
-			# wait time
-			time.sleep(3)
+            elif "Animation" in str(msg):
+                bot.send_animation(message.chat.id, file, reply_to_message_id=message.id)
+               
+            elif "Sticker" in str(msg):
+                bot.send_sticker(message.chat.id, file, reply_to_message_id=message.id)
+
+            elif "Voice" in str(msg):
+                bot.send_voice(message.chat.id, file, caption=msg.caption, thumb=thumb, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
+
+            elif "Audio" in str(msg):
+                try:
+                    with acc:
+                        thumb = acc.download_media(msg.audio.thumbs[0].file_id)
+                except:
+                    thumb = None
+                bot.send_audio(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])   
+                if thumb != None:
+                    os.remove(thumb)
+
+            elif "Photo" in str(msg):
+                bot.send_photo(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)
 
 
-# handle private
-def handle_private(message: pyrogram.types.messages_and_media.message.Message, chatid: int, msgid: int):
-		msg: pyrogram.types.messages_and_media.message.Message = acc.get_messages(chatid,msgid)
-		msg_type = get_message_type(msg)
+            os.remove(file)
+            if os.path.exists(f'{message.id}upstatus.txt'):
+                os.remove(f'{message.id}upstatus.txt')
+            bot.delete_messages(message.chat.id,[smsg.id])
+                
+        
+        # public
+        else:
+            username = datas[-2]
+            msg  = bot.get_messages(username,msgid)
+    
+            if "Document" in str(msg):
+                bot.send_document(message.chat.id, msg.document.file_id, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)
 
-		if "Text" == msg_type:
-			bot.send_message(message.chat.id, msg.text, entities=msg.entities, reply_to_message_id=message.id)
-			return
+            elif "Video" in str(msg):
+                bot.send_video(message.chat.id, msg.video.file_id, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)
+            
+            elif "Animation" in str(msg):
+                bot.send_animation(message.chat.id, msg.animation.file_id, reply_to_message_id=message.id)
 
-		smsg = bot.send_message(message.chat.id, '__Downloading__', reply_to_message_id=message.id)
-		dosta = threading.Thread(target=lambda:downstatus(f'{message.id}downstatus.txt',smsg),daemon=True)
-		dosta.start()
-		file = acc.download_media(msg, progress=progress, progress_args=[message,"down"])
-		os.remove(f'{message.id}downstatus.txt')
+            elif "Sticker" in str(msg):
+                bot.send_sticker(message.chat.id, msg.sticker.file_id, reply_to_message_id=message.id)
 
-		upsta = threading.Thread(target=lambda:upstatus(f'{message.id}upstatus.txt',smsg),daemon=True)
-		upsta.start()
-		
-		if "Document" == msg_type:
-			try:
-				thumb = acc.download_media(msg.document.thumbs[0].file_id)
-			except: thumb = None
-			
-			bot.send_document(message.chat.id, file, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-			if thumb != None: os.remove(thumb)
+            elif "Voice" in str(msg):
+                bot.send_voice(message.chat.id, msg.voice.file_id, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)    
 
-		elif "Video" == msg_type:
-			try: 
-				thumb = acc.download_media(msg.video.thumbs[0].file_id)
-			except: thumb = None
+            elif "Audio" in str(msg):
+                bot.send_audio(message.chat.id, msg.audio.file_id, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)    
 
-			bot.send_video(message.chat.id, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-			if thumb != None: os.remove(thumb)
+            elif "text" in str(msg):
+                bot.send_message(message.chat.id, msg.text, entities=msg.entities, reply_to_message_id=message.id)
 
-		elif "Animation" == msg_type:
-			bot.send_animation(message.chat.id, file, reply_to_message_id=message.id)
-			   
-		elif "Sticker" == msg_type:
-			bot.send_sticker(message.chat.id, file, reply_to_message_id=message.id)
+            elif "Photo" in str(msg):
+                bot.send_photo(message.chat.id, msg.photo.file_id, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)
 
-		elif "Voice" == msg_type:
-			bot.send_voice(message.chat.id, file, caption=msg.caption, thumb=thumb, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-
-		elif "Audio" == msg_type:
-			try:
-				thumb = acc.download_media(msg.audio.thumbs[0].file_id)
-			except: thumb = None
-				
-			bot.send_audio(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])   
-			if thumb != None: os.remove(thumb)
-
-		elif "Photo" == msg_type:
-			bot.send_photo(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)
-
-		os.remove(file)
-		if os.path.exists(f'{message.id}upstatus.txt'): os.remove(f'{message.id}upstatus.txt')
-		bot.delete_messages(message.chat.id,[smsg.id])
-
-
-# get the type of message
-def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
-	try:
-		msg.document.file_id
-		return "Document"
-	except: pass
-
-	try:
-		msg.video.file_id
-		return "Video"
-	except: pass
-
-	try:
-		msg.animation.file_id
-		return "Animation"
-	except: pass
-
-	try:
-		msg.sticker.file_id
-		return "Sticker"
-	except: pass
-
-	try:
-		msg.voice.file_id
-		return "Voice"
-	except: pass
-
-	try:
-		msg.audio.file_id
-		return "Audio"
-	except: pass
-
-	try:
-		msg.photo.file_id
-		return "Photo"
-	except: pass
-
-	try:
-		msg.text
-		return "Text"
-	except: pass
-
-
-USAGE = """**FOR PUBLIC CHATS**
-
-__just send post/s link__
-
-**FOR PRIVATE CHATS**
-
-__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__
-
-**FOR BOT CHATS**
-
-__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
-
-```
-https://t.me/b/botusername/4321
-```
-
-**MULTI POSTS**
-
-__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
-
-```
-https://t.me/xxxx/1001-1010
-
-https://t.me/c/xxxx/101 - 120
-```
-
-__note that space in between doesn't matter__
-"""
-
-
+print("Bot Started Successfully")
 # infinty polling
 bot.run()
